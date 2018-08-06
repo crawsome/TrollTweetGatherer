61bfd1b3b0190530f7490e06e3818c8aa267dad9
Colin <c3g6b2@gmail.com>
2018-08-05 20:48:59 -0700

    first upload - only searches tweets, nothing complicated yet

--- TrollTweetSearch.py
+++ TrollTweetSearch.py
@@ -1,40 +1,33 @@
-# Goes through list and greps out subjects and prints them all pretty
+# Goes through list and greps out queries and prints them all pretty
 # CSV Source: https://github.com/fivethirtyeight/russian-troll-tweets/
 
 import os
-
 import csv
 
 print('Troll Tweet Finder by Colin Burke:\n')
+query = str(input('Please enter search string: '))
 
-subject = str(input('Please enter search string: '))
-
+# collections
 ourlist = next(os.walk('tweets/'))[2]
-
 tweetlist = []
-
 authorlist = []
 
+# Retrieving results section:
 for tweetfile, index in zip(ourlist, range(0, len(ourlist))):
     print('Now searching through ' + str(tweetfile) + '...')
     with open('./tweets/' + str(tweetfile), encoding="utf8") as fin:
         dr = csv.DictReader(fin)
         for row in dr:
-            if subject.lower() in row['content'].lower():
+            if query.lower() in row['content'].lower():
                 tweetlist.append(row['content'])
                 authorlist.append(row['author'])
-
 print(str(len(tweetlist)) + ' results found.')
 
-for tweet, author in zip(tweetlist, authorlist):
-    print('Troll account name: ' + str(author))
-    print('Tweet: \n' + str(tweet) + '\n')
-
 """ROWS FOR QUERYING YOURSELF: 
-            row['external_author_id'], 
-            row['author'], 
-            row['content'], 
-            row['region'],
+          row['external_author_id'], 
+          row['author'], 
+          row['content'], 
+          row['region'],
           row['language'], 
           row['publish_date'], 
           row['harvested_date'], 
@@ -47,3 +40,9 @@ for tweet, author in zip(tweetlist, authorlist):
           row['account_category'], 
           row['new_june_2018'])
           """
+
+# Processing results section:
+for tweet, author in zip(tweetlist, authorlist):
+    print('Troll account name: ' + str(author))
+    print('Tweet: \n' + str(tweet) + '\n')
+
---
+++ tweets/README.txt
@@ -0,0 +1,9 @@
+About this Script:
+Colin Burke 2018
+A simple CSV setup as a way of searching through the CSVs of these 3 million troll tweets.
+The dataset is not mine, but it is included here for convenience.
+
+
+About the dataset in ./tweets/:
+Obtained data from: https://fivethirtyeight.com/features/why-were-sharing-3-million-russian-troll-tweets/
+From: https://github.com/fivethirtyeight/russian-troll-tweets/
